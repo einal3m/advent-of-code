a6314a513dd3dab1debbd4525b805a537372331e
Melanie Matthews <melanie.matthews@cultureamp.com>
2022-04-20 20:30:22 +1000

    day 01, 2021

---
+++ common/file.go
@@ -0,0 +1,20 @@
+package file
+
+import (
+	"io/ioutil"
+	"strconv"
+	"strings"
+)
+
+func ReadPuzzleInputInts(delimiter string) []int {
+	content, _ := ioutil.ReadFile("input.txt")
+	lines := strings.Split(string(content), delimiter)
+
+	var puzzleInput = []int{}
+	for _, value := range lines {
+		intValue, _ := strconv.Atoi(value)
+		puzzleInput = append(puzzleInput, intValue)
+	}
+
+	return puzzleInput
+}
